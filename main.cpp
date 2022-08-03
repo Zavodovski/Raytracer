68e83464484869d7b42f17faf3d669ff2e3cad90
Zavodovski <lqhheart@gmail.com>
2022-08-03 14:35:46 +0800

    Volumes

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include "bvh.h"
 #include "arrect.h"
 #include "box.h"
+#include "constant_medium.h"
 
 //std
 #include <iostream>
@@ -170,6 +171,41 @@ hittable_list cornell_box()
     return objects;
 }
 
+hittable_list cornell_smoke()
+{
+    hittable_list objects;
+
+    auto red = std::make_shared<lambertian>(color(0.65, 0.05, 0.05));
+    auto white = std::make_shared<lambertian>(color(0.73, 0.73, 0.73));
+    auto green = std::make_shared<lambertian>(color(0.12, 0.45, 0.15));
+    auto light = std::make_shared<diffuse_light>(color(7, 7, 7));
+
+    objects.add(std::make_shared<yz_rect>(0, 555, 0, 555, 555, green));
+    objects.add(std::make_shared<yz_rect>(0, 555, 0, 555, 0, red));
+    objects.add(std::make_shared<xz_rect>(113, 443, 127, 432, 554, light));
+    objects.add(std::make_shared<xz_rect>(0, 555, 0, 555, 0, white));
+    objects.add(std::make_shared<xz_rect>(0, 555, 0, 555, 555, white));
+    objects.add(std::make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+
+    //objects.add(std::make_shared<box>(point3(130, 0, 65), point3(295, 165, 230), white));
+    //objects.add(std::make_shared<box>(point3(265, 0, 295), point3(430, 330, 460), white));
+
+    std::shared_ptr<hittable> box1 = std::make_shared<box>(point3(0, 0, 0), point3(165, 330, 165), white);
+    box1 = std::make_shared<rotate_y>(box1, 15);
+    box1 = std::make_shared<translate>(box1, vec3(265, 0, 295));
+    //objects.add(box1);
+
+    std::shared_ptr<hittable> box2 = std::make_shared<box>(point3(0, 0, 0), point3(165, 165, 165), white);
+    box2 = std::make_shared<rotate_y>(box2, -18);
+    box2 = std::make_shared<translate>(box2, vec3(130, 0, 65));
+    //objects.add(box2);
+
+    objects.add(std::make_shared<constant_medium>(box1, 0.01, color(0, 0, 0)));
+    objects.add(std::make_shared<constant_medium>(box2, 0.01, color(1, 1, 1)));
+    
+    return objects;
+}
+
 int main()
 {
     // Image
@@ -243,7 +279,7 @@ int main()
             lookat = point3(0, 2, 0);
             vfov = 20.0;
             break;
-        default:
+        
         case 6:
             world = cornell_box();
             aspect_ratio = 1.0;
@@ -253,8 +289,18 @@ int main()
             lookfrom = point3(278, 278, -800);
             lookat = point3(278, 278, 0);
             vfov = 40.0;
-            break;          
-
+            break; 
+        default:  
+        case 7:
+            world = cornell_smoke();
+            aspect_ratio = 1.0;
+            image_width = 800;
+            samples_per_pixel = 1000;
+            background = color(0, 0, 0);
+            lookfrom = point3(278, 278, -800);
+            lookat = point3(278, 278, 0);
+            vfov = 40.0;
+            break;         
     }
 
     //Camera
